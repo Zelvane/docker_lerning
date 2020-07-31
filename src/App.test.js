d5b0840b33b2e0de2e808bb55c90e37cf6b34382
Egorov Maksim <m.a.egoro@Gmail.com>
2020-07-31 04:13:31 -0400

    second commit

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,6 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
